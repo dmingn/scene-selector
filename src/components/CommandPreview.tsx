e4b95ec5c67af9fb66a975e981b1ef06b9cf4061
dmingn <dmingn@users.noreply.github.com>
2024-12-26 02:45:22 +0900

    getOutPath の修正とテストの追加

--- src/components/CommandPreview.tsx
+++ src/components/CommandPreview.tsx
@@ -2,19 +2,7 @@
 import { css } from '@emotion/react';
 import { TextField } from '@mui/material';
 import { frameNumberToTimecode } from '../utils/frameNumberToTimecode';
-
-const getOutPath = (
-  filePath: string,
-  startFrameNumber: number,
-  endFrameNumber: number,
-) => {
-  const dir_and_base = filePath.split('.', 2)[0];
-  const ext = filePath.split('.', 2)[1];
-
-  const modifiedBase = `${dir_and_base}-${startFrameNumber}-${endFrameNumber}`;
-
-  return `${modifiedBase}.${ext}`;
-};
+import { getOutPath } from '../utils/getOutPath';
 
 export const CommandPreview = (props: {
   filePath: string;
