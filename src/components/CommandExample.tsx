718cee4e1a68565742f1f4787f330a63e8d9dbcc
dmingn <dmingn@users.noreply.github.com>
2024-12-27 02:36:30 +0900

    クリックでコマンドをコピーする

--- src/components/CommandExample.tsx
+++ src/components/CommandExample.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { TextField } from '@mui/material';
+import { TextField, Tooltip } from '@mui/material';
+import { useState } from 'react';
 import { frameNumberToTimecode } from '../utils/frameNumberToTimecode';
 import { getOutPath } from '../utils/getOutPath';
 
@@ -26,21 +27,33 @@ export const CommandExample = (props: {
     `"${getOutPath(props.filePath, props.startFrameNumber, props.endFrameNumber)}"`,
   ].join(' ');
 
+  const [copied, setCopied] = useState(false);
+
   return (
     <div css={css({ width: '100%' })}>
-      <TextField
-        type="text"
-        label="FFmpeg command example"
-        value={command}
-        slotProps={{
-          input: {
-            readOnly: true,
-          },
-          inputLabel: { shrink: true },
-        }}
-        css={css({ width: '100%' })}
-        multiline
-      />
+      <Tooltip
+        placement="top"
+        title={copied ? 'Copied' : 'Click to copy'}
+        onClose={() => setCopied(false)}
+      >
+        <TextField
+          type="text"
+          label="FFmpeg command example"
+          value={command}
+          slotProps={{
+            input: {
+              readOnly: true,
+            },
+            inputLabel: { shrink: true },
+          }}
+          css={css({ width: '100%' })}
+          multiline
+          onClick={() => {
+            navigator.clipboard.writeText(command);
+            setCopied(true);
+          }}
+        />
+      </Tooltip>
     </div>
   );
 };
