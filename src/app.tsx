dce197eab745b7d93c42f2b402d0523aa5de2b8c
dmingn <dmingn@users.noreply.github.com>
2024-12-27 11:46:16 +0900

    レイアウトの調整

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { CircularProgress } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ipcLink } from 'electron-trpc/renderer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { CommandExample } from './components/CommandExample';
 import { FileInput } from './components/FileInput';
 import { StartEndSelector } from './components/StartEndSelector';
 import { trpc } from './trpc';
@@ -19,10 +21,74 @@ const Content = () => {
     }
   }
 
+  const [frameCount, setFrameCount] = useState<number>(0);
+  const [fps, setFps] = useState<number>(0);
+
+  const { data: videoInfo } = trpc.getVideoInfo.useQuery({
+    path: filePath,
+  });
+
+  useEffect(() => {
+    if (videoInfo) {
+      setFrameCount(videoInfo.frameCount);
+      setFps(videoInfo.fps);
+    }
+  }, [videoInfo]);
+
+  const [startFrameNumber, _setStartFrameNumber] = useState<number>(0);
+  const setStartFrameNumber = (newValue: number) => {
+    _setStartFrameNumber(Math.max(0, Math.min(endFrameNumber, newValue)));
+  };
+
+  const [endFrameNumber, _setEndFrameNumber] = useState<number>(0);
+  const setEndFrameNumber = (newValue: number) => {
+    _setEndFrameNumber(
+      Math.max(startFrameNumber, Math.min(frameCount - 1, newValue)),
+    );
+  };
+
+  useEffect(() => {
+    if (frameCount) {
+      setEndFrameNumber(frameCount);
+    }
+  }, [frameCount]);
+
   return (
-    <div css={css({ display: 'flex', flexDirection: 'column' })}>
+    <div
+      css={css({
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: filePath ? 'space-between' : 'center',
+        gap: '16px',
+        height: '100%',
+      })}
+    >
       <FileInput value={filePath} onChange={handleOnChange} />
-      {filePath && <StartEndSelector filePath={filePath} />}
+      {filePath &&
+        (videoInfo ? (
+          <StartEndSelector
+            filePath={filePath}
+            fps={fps}
+            frameCount={frameCount}
+            startFrameNumber={startFrameNumber}
+            setStartFrameNumber={setStartFrameNumber}
+            endFrameNumber={endFrameNumber}
+            setEndFrameNumber={setEndFrameNumber}
+          />
+        ) : (
+          <CircularProgress />
+        ))}
+      {filePath &&
+        (videoInfo ? (
+          <CommandExample
+            filePath={filePath}
+            fps={fps}
+            startFrameNumber={startFrameNumber}
+            endFrameNumber={endFrameNumber}
+          />
+        ) : (
+          <></>
+        ))}
     </div>
   );
 };
