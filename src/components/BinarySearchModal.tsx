97c6ec7861265feb3533c58158ea673ab604b071
dmingn <dmingn@users.noreply.github.com>
2024-12-28 13:39:57 +0900

    フレームを二分探索するモーダルの追加

---
+++ src/components/BinarySearchModal.tsx
@@ -0,0 +1,136 @@
+/** @jsxImportSource @emotion/react */
+import { css, SerializedStyles } from '@emotion/react';
+import { Button, Dialog } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
+import { VideoContext } from '../contexts/VideoContext';
+import { frameNumberToTimecode } from '../utils/frameNumberToTimecode';
+import { FrameImage } from './FrameImage';
+
+const FrameImageAndSelectButton = (props: {
+  fps: number;
+  frameNumber: number;
+  onSelect: (value: number) => void;
+  className?: string;
+  css?: SerializedStyles;
+}) => {
+  return (
+    <div
+      className={props.className}
+      css={[
+        css({
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '4px',
+        }),
+        props.css,
+      ]}
+    >
+      <FrameImage frameNumber={props.frameNumber} />
+      <div>{props.frameNumber}</div>
+      <div>{frameNumberToTimecode(props.frameNumber, props.fps)}</div>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          props.onSelect(props.frameNumber);
+        }}
+      >
+        Select
+      </Button>
+    </div>
+  );
+};
+
+export const BinarySearchModal = (props: {
+  open: boolean;
+  onClose: (value: number) => void;
+  leftFrameNumber: number;
+  rightFrameNumber: number;
+}) => {
+  const { fps } = useContext(VideoContext);
+
+  const [leftFrameNumber, setLeftFrameNumber] = useState<number>(0);
+  const [midFrameNumber, setMidFrameNumber] = useState<number>(0);
+  const [rightFrameNumber, setRightFrameNumber] = useState<number>(0);
+
+  useEffect(() => {
+    setMidFrameNumber(Math.floor((leftFrameNumber + rightFrameNumber) / 2));
+  }, [leftFrameNumber, rightFrameNumber]);
+
+  useEffect(() => {
+    if (props.open) {
+      setLeftFrameNumber(props.leftFrameNumber);
+      setRightFrameNumber(props.rightFrameNumber);
+    }
+  }, [props.open]);
+
+  return (
+    <Dialog open={props.open} fullWidth maxWidth="xl">
+      <div
+        css={css({
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '8px',
+          padding: '16px',
+        })}
+      >
+        <div
+          css={css({
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            gap: '8px',
+          })}
+        >
+          <FrameImageAndSelectButton
+            fps={fps}
+            frameNumber={leftFrameNumber}
+            onSelect={props.onClose}
+            css={css({ flex: 1 })}
+          />
+          <FrameImageAndSelectButton
+            fps={fps}
+            frameNumber={midFrameNumber}
+            onSelect={props.onClose}
+            css={css({ flex: 1 })}
+          />
+
+          <FrameImageAndSelectButton
+            fps={fps}
+            frameNumber={rightFrameNumber}
+            onSelect={props.onClose}
+            css={css({ flex: 1 })}
+          />
+        </div>
+        <div
+          css={css({
+            display: 'flex',
+            justifyContent: 'space-between',
+            gap: '8px',
+            width: '60%',
+          })}
+        >
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setRightFrameNumber(midFrameNumber);
+            }}
+            css={css({ flex: 1 })}
+          >
+            L
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setLeftFrameNumber(midFrameNumber);
+            }}
+            css={css({ flex: 1 })}
+          >
+            R
+          </Button>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
--- src/components/FrameView.tsx
+++ src/components/FrameView.tsx
@@ -1,14 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css, SerializedStyles } from '@emotion/react';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+import { BinarySearchModal } from './BinarySearchModal';
 import { FrameImage } from './FrameImage';
 import { FrameNumberControl } from './FrameNumberControl';
 
 export const FrameView = (props: {
   frameNumber: number;
   setFrameNumber: (frameNumber: number) => void;
+  frameNumberMin: number;
+  frameNumberMax: number;
   className?: string;
   css?: SerializedStyles;
 }) => {
+  const [openBinarySearchModal, setOpenBinarySearchModal] = useState(false);
+
   return (
     <div
       className={props.className}
@@ -25,6 +32,23 @@ export const FrameView = (props: {
       <FrameNumberControl
         frameNumber={props.frameNumber}
         setFrameNumber={props.setFrameNumber}
+      />
+      <Button
+        variant="outlined"
+        onClick={() => {
+          setOpenBinarySearchModal(true);
+        }}
+      >
+        Binary Search
+      </Button>
+      <BinarySearchModal
+        open={openBinarySearchModal}
+        onClose={(value) => {
+          setOpenBinarySearchModal(false);
+          props.setFrameNumber(value);
+        }}
+        leftFrameNumber={props.frameNumberMin}
+        rightFrameNumber={props.frameNumberMax}
       />
     </div>
   );
